3f6610485af83bf492170e0022dba8b4d4231f8b
sangpham97 <wunha1997@gmail.com>
2021-08-26 19:47:05 +0700

    fixed

--- src/components/BreadCums.jsx
+++ src/components/BreadCums.jsx
@@ -18,43 +18,12 @@ const useStyles = makeStyles(theme => ({
 export default function Breadcrumb({ category }) {
   const classes = useStyles()
   const arrays = category.split(",")
-  const data = useStaticQuery(graphql`
-    {
-      allContentfulMovies {
-        nodes {
-          title
-          engName
-          star
-          category
-          image {
-            gatsbyImageData(layout: CONSTRAINED, aspectRatio: 0.9)
-            title
-          }
-        }
-      }
-    }
-  `)
-  const movies = data.allContentfulMovies.nodes
-  // const arr = movies.map(movie => movie.category.split(","))
-  // console.log(arr)
-
-  const handleClick = (i, movies, e) => {
-    e.preventDefault()
-    const findMovie = movies.filter(movie =>
-      movie.category.split(",").includes(i)
-    )
-
-    console.log(findMovie)
-    navigate("/category", { state: { findMovie, i } })
-  }
 
   return (
     <Breadcrumbs aria-label="breadcrumb" className={classes.root}>
       <Link color="textPrimary">Xem phim</Link>
       {arrays.map((i, index) => (
-        <Link color="textPrimary" onClick={e => handleClick(i, movies, e)}>
-          {i}
-        </Link>
+        <Link color="textPrimary">{i}</Link>
       ))}
     </Breadcrumbs>
   )
